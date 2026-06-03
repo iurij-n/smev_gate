e01dc740f30e742f547d7c11cf9bbcbbf9b8436a
iurij-n <iurij.novickov2016@yandex.ru>
2026-06-03 17:56:57 +0300

    Поменял запрос

--- app/main.py
+++ app/main.py
@@ -82,13 +82,14 @@ async def execution_documents_send(payload: dict):
         "requestMessage": {
             "messageType": "RequestMessageType",
             "requestMetadata": {
-                "clientId": "00000000-0000-0000-0000-000000000020",
+                "clientId": "CLIENT_ID",
                 "testMessage": True
             },
             "requestContent": {
                 "content": {
                     "messagePrimaryContent": {
-                        "any": "<?xml version='1.0' encoding='UTF-8'?><fssp:NsiRequest xmlns:fssp='urn://x-artifacts-fssp-ru/mvv/smev3/nsi/1.0.1' xmlns:c='urn://x-artifacts-fssp-ru/mvv/smev3/container/1.0.1'><c:ID>2345</c:ID><c:Date>2015-10-12T00:00:00</c:Date><c:SenderOrganizationCode>БМ</c:SenderOrganizationCode><c:ReceiverOrganizationCode>ФССП</c:ReceiverOrganizationCode><c:Document><c:Organization>БМ</c:Organization><c:ID>2345</c:ID><c:Type>ReferenceInfoRequest</c:Type><c:DocumentDate>2015-10-12</c:DocumentDate><c:DocumentNumber>2345</c:DocumentNumber><c:Filename>req_4xzb12c8-2194-4401-8b49-49215ca1cfbc.zip</c:Filename></c:Document></fssp:NsiRequest>"
+                        # "any": "<?xml version='1.0' encoding='UTF-8'?><fssp:NsiRequest xmlns:fssp='urn://x-artifacts-fssp-ru/mvv/smev3/nsi/1.0.1' xmlns:c='urn://x-artifacts-fssp-ru/mvv/smev3/container/1.0.1'><c:ID>2345</c:ID><c:Date>2015-10-12T00:00:00</c:Date><c:SenderOrganizationCode>БМ</c:SenderOrganizationCode><c:ReceiverOrganizationCode>ФССП</c:ReceiverOrganizationCode><c:Document><c:Organization>БМ</c:Organization><c:ID>2345</c:ID><c:Type>ReferenceInfoRequest</c:Type><c:DocumentDate>2015-10-12</c:DocumentDate><c:DocumentNumber>2345</c:DocumentNumber><c:Filename>req_4xzb12c8-2194-4401-8b49-49215ca1cfbc.zip</c:Filename></c:Document></fssp:NsiRequest>"
+                        "any": "<?xml version='1.0' encoding='UTF-8'?><fssp:ApplicationDocumentsRequest xmlns:fssp='urn://x-artifacts-fssp-ru/mvv/smev3/application-documents/1.1.1' xmlns:c='urn://x-artifacts-fssp-ru/mvv/smev3/container/1.1.0' xmlns:att='urn://x-artifacts-fssp-ru/mvv/smev3/attachments/1.1.0'><c:ID>12f1a3cd-cd5f-48fe-8ed4-f73e3d9f9b2b</c:ID><c:Date>2015-10-12T00:00:00</c:Date><c:SenderID>EPGU01</c:SenderID><c:ReceiverID>FSSP01</c:ReceiverID><c:ReceiverDepartmentCode>33013</c:ReceiverDepartmentCode><c:Document><c:ID>2469</c:ID><c:Type>I_IPSIDE_OSP_COURSEIP</c:Type><c:DocumentDate>2015-10-12</c:DocumentDate><c:DocumentNumber>506559</c:DocumentNumber><c:DocumentCaseNumber>7407/14/33025-ИП</c:DocumentCaseNumber><c:AttachmentsBlock><att:AttachmentDescription><att:AttachmentFormat><att:IsUnstructuredFormat>false</att:IsUnstructuredFormat><att:IsZippedPacket>true</att:IsZippedPacket><att:StructuredFormatType>http://www.fssprus.ru/namespace/IRequestOther/2017/1</att:StructuredFormatType></att:AttachmentFormat><att:AttachmentFilename>piev_4ccb83c6-2888-4561-8b49-49215ca1cfbc.zip</att:AttachmentFilename></att:AttachmentDescription></c:AttachmentsBlock></c:Document></fssp:ApplicationDocumentsRequest>"
                     }
                 }
             }
